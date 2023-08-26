b435a993b73c8c725c3e44c32a4cbb8c5a0466e7
Abbaswazzan <wazzanabbas@gmail.com>
2023-08-26 20:29:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,26 @@
 
 
 # BONUS: Print the area in the following syntax: "If the length of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
+name = "Abbas"
+print(name)
+juice_price = 4
+chocolate_price = 4
+print(juice_price + chocolate_price)
+carrot_price = 5
+cucumber_price = 5
+print(carrot_price / cucumber_price)
+potato_price = 6
+onion_price = 6
+print(potato_price * onion_price)
+#part 2
+length = 5
+width = 3
+area = length * width
+print(f"The ruslt is {area}")
+#bonus
+print("If the length of the rectangle is 5, and width is 3, then the area is 15")
+
+
+
+
+
